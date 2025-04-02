df7a1233e7222264b1815feed1330f64e5ef874c
Oleh Tatarynov <exocriador@gmail.com>
2025-04-02 22:20:04 +0300

    update

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,16 +1,30 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import ArticlesList from "../ArticlesList/ArticlesList";
+import { fetchArticlesWithTopic } from "../../articles-api";
 
 const News = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     async function fetchArticles() {
-      const response = await axios.get(
-        "https://hn.algolia.com/api/v1/search?query=react"
-      );
-      setArticles(response.data.hits);
-      console.log(response);
+      try {
+        // 1. Встановлюємо індикатор в true перед запитом
+        setLoading(true);
+        const response = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+        setError(true);
+        console.error("Error fetching articles:", error);
+        // Тут будемо обробляти помилку
+      } finally {
+        // 2. Встановлюємо індикатор в false після запиту
+        setLoading(false);
+      }
     }
 
     fetchArticles();
@@ -18,7 +32,11 @@ const News = () => {
 
   return (
     <div>
-      <h1>Cambala articles</h1>
+      <h1>Latest articles</h1>
+      {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
       {articles.length > 0 && <ArticlesList items={articles} />}
     </div>
   );
